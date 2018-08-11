d94d0df5451dfe590c1f0db6d1fbf01a59b4aba5
sunyanping <sunyanping24@sina.com>
2018-08-11 09:34:20 +0800

    spring cloud

---
+++ common/src/main/java/cn/qidd/keep/common/utils/package-info.java
@@ -0,0 +1,1 @@
+package cn.qidd.keep.common.utils;
---
+++ eureka/src/main/java/cn/qidd/keep/EurekaApplication.java
@@ -0,0 +1,33 @@
+package cn.qidd.keep;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(EurekaApplication.class);
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication application = new SpringApplication(EurekaApplication.class);
+        ConfigurableEnvironment env = application.run(args).getEnvironment();
+        logger.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:{}\n\t" +
+                        "External: \thttp://{}:{}\n\t" +
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                env.getProperty("server.port"));
+    }
+}
