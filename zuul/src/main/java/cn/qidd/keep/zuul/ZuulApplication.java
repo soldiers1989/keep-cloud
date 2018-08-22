a398e8ee44ef2a316577c9c3b925c3ffc7e06825
sunyanping <yanping.sun@fintecher.cn>
2018-08-22 11:54:39 +0800

    添加不同环境文件

--- zuul/src/main/java/cn/qidd/keep/zuul/ZuulApplication.java
+++ zuul/src/main/java/cn/qidd/keep/zuul/ZuulApplication.java
@@ -1,15 +1,36 @@
 package cn.qidd.keep.zuul;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringCloudApplication
 @EnableZuulProxy
 public class ZuulApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ZuulApplication.class, args);
+    private final static Logger logger = LoggerFactory.getLogger(ZuulApplication.class);
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication application = new SpringApplication(ZuulApplication.class);
+        ConfigurableEnvironment env = application.run(args).getEnvironment();
+        logger.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:{}\n\t" +
+                        "External: \thttp://{}:{}\n\t" +
+                        "Swagger-ui: \thttp://localhost:{}/swagger-ui.html\n\t" +
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                env.getProperty("server.port"),
+                env.getProperty("server.port"));
     }
 }
