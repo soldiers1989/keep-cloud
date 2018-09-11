73fff7d41e0ebc34fdbbe39c01bde77e2af9780e
sunyanping <yanping.sun@fintecher.cn>
2018-09-11 18:42:07 +0800

    login

---
+++ service/manage/src/main/java/cn/qidd/keep/manage/controller/LoginController.java
@@ -0,0 +1,14 @@
+package cn.qidd.keep.manage.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api
+@RestController
+@RequestMapping("/api/login")
+@ApiModel(description = "登陆")
+public class LoginController {
+
+}
